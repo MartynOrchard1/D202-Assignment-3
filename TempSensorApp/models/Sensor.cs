5db60a1ca14b589d420b630fb642fea4ce16197f
Marty <martorchard@gmail.com>
2024-11-09 14:46:23 +1300

    Setup basic model

--- TempSensorApp/models/Sensor.cs
+++ TempSensorApp/models/Sensor.cs
@@ -4,6 +4,10 @@ namespace TempSensor Models
 {
     public class Sensor 
     {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
 
     }
 }
