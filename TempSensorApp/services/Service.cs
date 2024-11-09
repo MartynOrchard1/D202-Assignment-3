df9ac0229eecdd526a54bd3c879cba35adfca512
Marty <martorchard@gmail.com>
2024-11-09 14:54:20 +1300

    Add comments

--- TempSensorApp/services/Service.cs
+++ TempSensorApp/services/Service.cs
@@ -11,8 +11,10 @@ namespace Sensor Services
     // Define Class
     public class sensorService
     {
+        // Random init
         private rand _random = new rand();
 
+        // Setup Sensor
         public Sensor initSensor(string name, string location, double minValue, double maxValue) 
         {
             return new Sensor { name = name, location = location, minValue = minValue, maxValue = maxValue }
