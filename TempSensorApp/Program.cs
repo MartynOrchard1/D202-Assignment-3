ae60e88fbacdc9b53940c3e35f8d3a3b4060fdc2
Marty <martorchard@gmail.com>
2024-11-09 14:39:08 +1300

    Setup variables and sensor simulation

--- TempSensorApp/Program.cs
+++ TempSensorApp/Program.cs
@@ -9,7 +9,11 @@ namespace TempSensor
     {
         static async Task Main(string[] args)
         {
+            var sensorService = new SensorServeice();
+            var sensor = sensorService.initSensor("Sensor 1", "Data Center", 22, 24)
 
+            console.writeline("Starting temperature sensor simulation...")
+            await sensorService.StartSensor(sensor);
         }
     }
 
