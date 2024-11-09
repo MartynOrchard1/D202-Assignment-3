791e34ec0dbeadd064aa16e26bf6b976668c4e25
Marty <martorchard@gmail.com>
2024-11-09 14:38:57 +1300

    Setup Namespace, define class

--- TempSensorApp/Program.cs
+++ TempSensorApp/Program.cs
@@ -3,3 +3,14 @@ using TempSensorApp.services;
 using TempSensorApp.models;
 using System.Threading.tasks;
 
+namespace TempSensor 
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+
+}
